c62a7465f1ed357fb0baacd23f21ecbf7aa03dab
Slither <slither@sindome.org>
2021-10-02 12:10:50 -0700

    turned off safe recycle

--- src/include/options.h
+++ src/include/options.h
@@ -501,7 +501,7 @@
  * you can disable this option for a speed boost in larger databases.
  ******************************************************************************
 */
-#define SAFE_RECYCLE
+/*#define SAFE_RECYCLE*/
 
 /******************************************************************************
  * Configurable options for the background subsystem.
