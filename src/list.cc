c5618231e430cf5647e4dbcb860a063b59cd043d
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2023-05-31 08:30:00 +0000

    Added pluralization function.

--- src/list.cc
+++ src/list.cc
@@ -20,6 +20,8 @@
 #include <algorithm> // std::sort
 #include "dependencies/strnatcmp.c" // natural sorting
 #include <vector>
+#include <regex>
+#include <boost/algorithm/string.hpp>
 
 #include <ctype.h>
 #include <string.h>
@@ -1035,6 +1037,273 @@ bf_all_members(Var arglist, Byte next, void *vdata, Objid progr)
     return background_thread(all_members_thread_callback, &arglist, human_string);
 }
 
+static const std::vector<std::regex> UNCOUNTABLE_RULES = {
+    std::regex("adulthood"), std::regex("advice"), std::regex("agenda"),
+    std::regex("aid"), std::regex("aircraft"), std::regex("alcohol"),
+    std::regex("ammo"), std::regex("analytics"), std::regex("anime"),
+    std::regex("athletics"), std::regex("audio"), std::regex("bison"),
+    std::regex("blood"), std::regex("bream"), std::regex("buffalo"),
+    std::regex("butter"), std::regex("carp"), std::regex("cash"),
+    std::regex("chassis"), std::regex("chess"), std::regex("clothing"),
+    std::regex("cod"), std::regex("commerce"), std::regex("cooperation"),
+    std::regex("corps"), std::regex("debris"), std::regex("diabetes"),
+    std::regex("digestion"), std::regex("elk"), std::regex("energy"),
+    std::regex("equipment"), std::regex("excretion"), std::regex("expertise"),
+    std::regex("firmware"), std::regex("flounder"), std::regex("fun"),
+    std::regex("gallows"), std::regex("garbage"), std::regex("graffiti"),
+    std::regex("hardware"), std::regex("headquarters"), std::regex("health"),
+    std::regex("herpes"), std::regex("highjinks"), std::regex("homework"),
+    std::regex("housework"), std::regex("information"), std::regex("jeans"),
+    std::regex("justice"), std::regex("kudos"), std::regex("labour"),
+    std::regex("literature"), std::regex("machinery"), std::regex("mackerel"),
+    std::regex("mail"), std::regex("media"), std::regex("mews"),
+    std::regex("moose"), std::regex("music"), std::regex("mud"),
+    std::regex("manga"), std::regex("news"), std::regex("only"),
+    std::regex("personnel"), std::regex("pike"), std::regex("plankton"),
+    std::regex("pliers"), std::regex("police"), std::regex("pollution"),
+    std::regex("premises"), std::regex("rain"), std::regex("research"),
+    std::regex("rice"), std::regex("salmon"), std::regex("scissors"),
+    std::regex("series"), std::regex("sewage"), std::regex("shambles"),
+    std::regex("shrimp"), std::regex("software"), std::regex("staff"),
+    std::regex("swine"), std::regex("tennis"), std::regex("traffic"),
+    std::regex("transportation"), std::regex("trout"), std::regex("tuna"),
+    std::regex("wealth"), std::regex("welfare"), std::regex("whiting"),
+    std::regex("wildebeest"), std::regex("wildlife"), std::regex("you"),
+    std::regex("pok[e]mon$"), std::regex("[^aeiou]ese$"), std::regex("deer$"),
+    std::regex("fish$"), std::regex("measles$"), std::regex("o[iu]s$")
+};
+
+static const std::map<std::string, std::string> IRREGULAR_SINGULAR_RULES = {
+    { "anathema", "anathemata" },
+    { "axe", "axes" },
+    { "canvas", "canvases" },
+    { "carve", "carves" },
+    { "die", "dice" },
+    { "dingo", "dingoes" },
+    { "dogma", "dogmata" },
+    { "eave", "eaves" },
+    { "echo", "echoes" },
+    { "foot", "feet" },
+    { "genus", "genera" },
+    { "go", "goes" },
+    { "goose", "geese" },
+    { "groove", "grooves" },
+    { "has", "have" },
+    { "he", "they" },
+    { "her", "their" },
+    { "herself", "themselves" },
+    { "himself", "themselves" },
+    { "his", "their" },
+    { "human", "humans" },
+    { "I", "we" },
+    { "is", "are" },
+    { "its", "their" },
+    { "itself", "themselves" },
+    { "lemma", "lemmata" },
+    { "looey", "looies" },
+    { "me", "us" },
+    { "my", "our" },
+    { "myself", "ourselves" },
+    { "ox", "oxen" },
+    { "passerby", "passersby" },
+    { "pickaxe", "pickaxes" },
+    { "proof", "proofs" },
+    { "quiz", "quizzes" },
+    { "schema", "schemata" },
+    { "she", "they" },
+    { "stigma", "stigmata" },
+    { "stoma", "stomata" },
+    { "that", "those" },
+    { "them", "them" },
+    { "themself", "themselves" },
+    { "thief", "thieves" },
+    { "this", "these" },
+    { "tooth", "teeth" },
+    { "tornado", "tornadoes" },
+    { "torpedo", "torpedoes" },
+    { "valve", "valves" },
+    { "viscus", "viscera" },
+    { "volcano", "volcanoes" },
+    { "was", "were" },
+    { "yes", "yeses" },
+    { "yourself", "yourselves" },
+};
+
+static const std::map<std::string, std::string> IRREGULAR_PLURAL_RULES = {
+    { "anathemata", "anathema" },
+    { "are", "is" },
+    { "axes", "axe" },
+    { "canvases", "canvas" },
+    { "carves", "carve" },
+    { "dice", "die" },
+    { "dingoes", "dingo" },
+    { "dogmata", "dogma" },
+    { "eaves", "eave" },
+    { "echoes", "echo" },
+    { "feet", "foot" },
+    { "geese", "goose" },
+    { "genera", "genus" },
+    { "goes", "go" },
+    { "grooves", "groove" },
+    { "have", "has" },
+    { "humans", "human" },
+    { "lemmata", "lemma" },
+    { "looies", "looey" },
+    { "our", "my" },
+    { "ourselves", "myself" },
+    { "oxen", "ox" },
+    { "passersby", "passerby" },
+    { "pickaxes", "pickaxe" },
+    { "proofs", "proof" },
+    { "quizzes", "quiz" },
+    { "schemata", "schema" },
+    { "stigmata", "stigma" },
+    { "stomata", "stoma" },
+    { "teeth", "tooth" },
+    { "their", "its" },
+    { "them", "them" },
+    { "themselves", "themself" },
+    { "these", "this" },
+    { "they", "she" },
+    { "thieves", "thief" },
+    { "those", "that" },
+    { "tornadoes", "tornado" },
+    { "torpedoes", "torpedo" },
+    { "us", "me" },
+    { "valves", "valve" },
+    { "viscera", "viscus" },
+    { "volcanoes", "volcano" },
+    { "we", "I" },
+    { "were", "was" },
+    { "yeses", "yes" },
+    { "yourselves", "yourself" }
+};
+
+static const std::map<std::regex, std::string> SINGULARIZATION_RULES = {
+    { std::regex("(agend|addend|millenni|dat|extrem|bacteri|desiderat|strat|candelabr|errat|ov|symposi|curricul|quor)a$"), "$1um" },
+    { std::regex("(alumn|alg|vertebr)ae$"), "$1a" },
+    { std::regex("(alumn|syllab|vir|radi|nucle|fung|cact|stimul|termin|bacill|foc|uter|loc|strat)(?:us|i)$"), "$1us" },
+    { std::regex("(analy|diagno|parenthe|progno|synop|the|empha|cri|ne)(?:sis|ses)$"), "$1sis" },
+    { std::regex("(apheli|hyperbat|periheli|asyndet|noumen|phenomen|criteri|organ|prolegomen|hedr|automat)a$"), "$1on" },
+    { std::regex("(ar|(?:wo|[ae])l|[eo][ao])ves$"), "$1f" },
+    { std::regex("(child)ren$"), "$1" },
+    { std::regex("(cod|mur|sil|vert|ind)ices$"), "$1ex" },
+    { std::regex("(dg|ss|ois|lk|ok|wn|mb|th|ch|ec|oal|is|ck|ix|sser|ts|wb)ies$"), "$1ie" },
+    { std::regex("(eau)x?$"), "$1" },
+    { std::regex("(matr|append)ices$"), "$1ix" },
+    { std::regex("(movie|twelve|abuse|e[mn]u)s$"), "$1" },
+    { std::regex("(pe)(rson|ople)$"), "$1rson" },
+    { std::regex("(seraph|cherub)im$"), "$1" },
+    { std::regex("(ss)$"), "$1" },
+    { std::regex("(test)(?:is|es)$"), "$1is" },
+    { std::regex("(wi|kni|(?:after|half|high|low|mid|non|night|[^w]|^)li)ves$"), "$1fe" },
+    { std::regex("(x|ch|ss|sh|zz|tto|go|cho|alias|[^aou]us|t[lm]as|gas|(?:her|at|gr)o|[aeiou]ris)(?:es)?$"), "$1" },
+    { std::regex("b((?:tit)?m|l)ice$"), "$1ouse" },
+    { std::regex("b(l|(?:neck|cross|hog|aun)?t|coll|faer|food|gen|goon|group|hipp|junk|vegg|(?:pork)?p|charl|calor|cut)ies$"), "$1ie" },
+    { std::regex("b(mon|smil)ies$"), "$1ey" },
+    { std::regex("ies$"), "y" },
+    { std::regex("men$"), "man" },
+    { std::regex("s$"), "" }
+};
+
+static const std::map<std::regex, std::string> PLURALIZATION_RULES = {
+    { std::regex("(?:(kni|wi|li)fe|(ar|l|ea|eo|oa|hoo)f)$"), "$1$2ves" },
+    { std::regex("([^aeiou]ese)$"), "$1" },
+    { std::regex("([^aeiouy]|qu)y$"), "$1ies" },
+    { std::regex("([^ch][ieo][ln])ey$"), "$1ies" },
+    { std::regex("([^l]ias|[aeiou]las|[ejzr]as|[iu]am)$"), "$1" },
+    { std::regex("(agend|addend|millenni|dat|extrem|bacteri|desiderat|strat|candelabr|errat|ov|symposi|curricul|automat|quor)(?:a|um)$"), "$1a" },
+    { std::regex("(alias|[^aou]us|t[lm]as|gas|ris)$"), "$1es" },
+    { std::regex("(alumn|alg|vertebr)(?:a|ae)$"), "$1ae" },
+    { std::regex("(alumn|syllab|vir|radi|nucle|fung|cact|stimul|termin|bacill|foc|uter|loc|strat)(?:us|i)$"), "$1i" },
+    { std::regex("(apheli|hyperbat|periheli|asyndet|noumen|phenomen|criteri|organ|prolegomen|hedr|automat)(?:a|on)$"), "$1a" },
+    { std::regex("(ax|test)is$"), "$1es" },
+    { std::regex("(child)(?:ren)?$"), "$1ren" },
+    { std::regex("(e[mn]u)s?$"), "$1s" },
+    { std::regex("(her|at|gr)o$"), "$1oes" },
+    { std::regex("(matr|cod|mur|sil|vert|ind|append)(?:ix|ex)$"), "$1ices" },
+    { std::regex("(pe)(?:rson|ople)$"), "$1ople" },
+    { std::regex("(seraph|cherub)(?:im)?$"), "$1im" },
+    { std::regex("(x|ch|ss|sh|zz)$"), "$1es" },
+    { std::regex("[^u0000-u007F]$"), "$0" },
+    { std::regex("b((?:tit)?m|l)(?:ice|ouse)$"), "$1ice" },
+    { std::regex("eaux$"), "$0" },
+    { std::regex("m[ae]n$"), "men" },
+    { std::regex("s?$"), "s" },
+    { std::regex("sisi"), "ses" },
+    { std::regex("thou"), "you" }
+};
+
+static package
+bf_pluralize(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    Var r;
+    r.type = TYPE_STR;
+
+    int quantity = 1;
+    if (arglist.v.list[0].v.num == 2)
+      quantity = arglist.v.list[2].v.num;
+    std::string token = boost::algorithm::to_lower_copy(arglist.v.list[1].v.str);
+
+    // check for uncountables
+    for (const std::regex &rule : UNCOUNTABLE_RULES) {
+        if (std::regex_match(token, rule)) {
+            r.v.str = str_dup(arglist.v.list[1].v.str);
+            free_var(arglist);
+            return make_var_pack(r);
+        }
+    }
+
+    // check for irregulars
+    if (quantity == 1) {
+        for (const auto& pair : IRREGULAR_SINGULAR_RULES) {
+            if (token == pair.first) {
+                r.v.str = str_dup(arglist.v.list[1].v.str);
+                free_var(arglist);
+                return make_var_pack(r);
+            }
+        }
+        for (const auto& pair : IRREGULAR_PLURAL_RULES) {
+            if (token == pair.first) {
+                r.v.str = str_dup(pair.second.c_str());
+                free_var(arglist);
+                return make_var_pack(r);
+            }
+        }
+    } else {
+        for (const auto& pair : IRREGULAR_PLURAL_RULES) {
+            if (token == pair.first) {
+                r.v.str = str_dup(arglist.v.list[1].v.str);
+                free_var(arglist);
+                return make_var_pack(r);
+            }
+        }
+        for (const auto& pair : IRREGULAR_SINGULAR_RULES) {
+            if (token == pair.first) {
+                r.v.str = str_dup(pair.second.c_str());
+                free_var(arglist);
+                return make_var_pack(r);
+            }
+        }
+    }
+
+    // Guard checks over, now to do actual replacements
+    for (const auto& pair : quantity == 1 ? SINGULARIZATION_RULES : PLURALIZATION_RULES) {
+        std::string result = std::regex_replace(token, pair.first, pair.second);
+        if (result != token) {
+            // We had a replacement.
+            r.v.str = str_dup(result.c_str());
+            free_var(arglist);
+            return make_var_pack(r);
+        }
+    }
+
+    // No matches found the word is already correct.
+    r.v.str = str_dup(arglist.v.list[1].v.str);
+    free_var(arglist);
+    return make_var_pack(r);
+}
+
 static package
 bf_strsub(Var arglist, Byte next, void *vdata, Objid progr)
 {   /* (source, what, with [, case-matters]) */
@@ -1654,6 +1923,7 @@ register_list(void)
     register_function("rindex", 2, 4, bf_rindex,
                       TYPE_STR, TYPE_STR, TYPE_ANY, TYPE_INT);
     register_function("strcmp", 2, 2, bf_strcmp, TYPE_STR, TYPE_STR);
+    register_function("pluralize", 1, 2, bf_pluralize, TYPE_STR, TYPE_INT);
     register_function("strsub", 3, 4, bf_strsub,
                       TYPE_STR, TYPE_STR, TYPE_STR, TYPE_ANY);
     register_function("strtr", 3, 4, bf_strtr,
