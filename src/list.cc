1b6213549b5b175010a7d8fd0a25d7c7e188bdd1
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2023-06-15 21:37:21 +0000

    Fixed off by one error and a memory leak, cleaned up complex match

--- src/list.cc
+++ src/list.cc
@@ -1305,93 +1305,72 @@ bf_pluralize(Var arglist, Byte next, void *vdata, Objid progr)
     return make_var_pack(r);
 }
 
+
+static Var
+compile_keys(Var *targets)
+{
+    Var keys = new_list(0);
+    Var target_keys;
+    for (int i = 1; i <= targets->v.list[0].v.num; i++) {
+        switch(targets->v.list[i].type) {
+            case TYPE_STR:
+                target_keys = new_list(1);
+                target_keys.v.list[1] = str_dup_to_var(targets->v.list[i].v.str);
+                keys = listappend(keys, target_keys);
+                break;
+            case TYPE_OBJ:
+                keys = listappend(keys, name_and_aliases(NOTHING, targets->v.list[i].v.obj));
+                break;
+            case TYPE_LIST:
+                target_keys = new_list(targets->v.list[i].v.list[0].v.num);
+                for (int i2 = 1; i2 <= target_keys.v.list[0].v.num; i2++) {
+                    switch (targets->v.list[i].v.list[i2].type) {
+                        case TYPE_STR:
+                            target_keys.v.list[i2] = str_dup_to_var(targets->v.list[i].v.list[i2].v.str);
+                            break;
+                        default:
+                            free_var(target_keys);
+                            free_var(keys);
+                            keys = new_list(0);
+                            return keys;
+                    }
+                }
+                keys = listappend(keys, target_keys);
+                break;
+            default:
+                free_var(keys);
+                keys = new_list(0);
+                return keys;
+        }
+    }
+    return keys;
+}
+
 static package
 bf_complex_match(Var arglist, Byte next, void *vdata, Objid progr)
 {   /* (subject, targets [, keys]) */
-    Objid result = match_object(69, arglist.v.list[1].v.str);
-    Var results = new_list(1);
-    results.v.list[1] = Var::new_obj(result);
-    Var keys = new_list(0);
 
     if (arglist.v.list[0].v.num == 3) {
         // There must be as many keys as there are targets.
         if (arglist.v.list[3].v.list[0].v.num != arglist.v.list[2].v.list[0].v.num) {
             free_var(arglist);
-            free_var(keys);
             return make_error_pack(E_INVARG);
-        }
-        // Compile our keys...
-        for (int i = 1; i <= arglist.v.list[3].v.list[0].v.num; i++) {
-            // std::vector<std::string> key_index;
-            Var target_keys = new_list(0);
-            switch(arglist.v.list[3].v.list[i].type) {
-                case TYPE_STR:
-                    target_keys = listappend(target_keys, var_dup(arglist.v.list[3].v.list[i]));
-                    break;
-                case TYPE_LIST:
-                    // Someone in the future look at this and go 'what the fuck is actually happening here'
-                    // ..possibly me..
-                    // TL;DR we support lists of lists to alias one key result to multiple strings
-                    // meaning one index target can have multiple strings that will match to it
-                    // this lets things like .aliases work.
-                    for (int x = 1; x <= arglist.v.list[3].v.list[i].v.list[0].v.num; x++) {
-                        if (arglist.v.list[3].v.list[i].v.list[x].type != TYPE_STR) {
-                            free_var(target_keys);
-                            free_var(keys);
-                            free_var(arglist);
-                            return make_error_pack(E_INVARG);
-                        }
-                        target_keys = listappend(target_keys, var_dup(arglist.v.list[3].v.list[i].v.list[x]));
-                    }
-                    break;
-                default:
-                    free_var(target_keys);
-                    free_var(keys);
-                    free_var(arglist);
-                    return make_error_pack(E_INVARG);
-            }
-            keys = listappend(keys, var_ref(target_keys));
-        }
-    } else {
-        // We have to calculate what the likely keys are based on the targets.
-        Var *names;
-        for (int i = 1; i <= arglist.v.list[2].v.list[0].v.num; i++) {
-            // If it's an object we'll use their aliases
-            // if it's a string we can assume it's the string itself.
-            Var target_keys = new_list(0);
-            switch (arglist.v.list[2].v.list[i].type) {
-                case TYPE_STR:
-                    target_keys = listappend(target_keys, var_dup(arglist.v.list[2].v.list[i]));
-                    break;
-                case TYPE_OBJ:
-                    target_keys = name_and_aliases(NOTHING, arglist.v.list[2].v.list[i].v.obj);
-                    break;
-                case TYPE_LIST:
-                    for (int x = 1; x <= arglist.v.list[2].v.list[i].v.list[0].v.num; x++) {
-                        if (arglist.v.list[2].v.list[i].v.list[x].type != TYPE_STR) {
-                            free_var(arglist);
-                            free_var(keys);
-                            free_var(target_keys);
-                            return make_error_pack(E_INVARG);
-                        }
-                        target_keys = listappend(target_keys, var_dup(arglist.v.list[2].v.list[i].v.list[x]));
-                    }
-                    break;
-                default:
-                    free_var(arglist);
-                    free_var(target_keys);
-                    free_var(keys);
-                    return make_error_pack(E_INVARG);
-            }
-            keys = listappend(keys, var_ref(target_keys));
         }
     }
 
-    // Now that we have our keys parsed out, the only thing left to do is run a match.
+    Var target_keys = arglist.v.list[0].v.num == 3 ? arglist.v.list[3] : arglist.v.list[2];
+    Var keys = compile_keys(&target_keys);
+    if (keys.v.list[0].v.num <= 0) {
+        free_var(arglist);
+        free_var(keys);
+        return make_error_pack(E_INVARG);
+    }
+
+    // // Now that we have our keys parsed out, the only thing left to do is run a match.
     std::vector<int> matches = complex_match(str_dup(arglist.v.list[1].v.str), &keys);
     Var r = new_list(0);
     for (int i=0;i < matches.size();i++) {
-        r = listappend(r, var_ref(arglist.v.list[2].v.list[matches[i] + 1]));
+        r = listappend(r, var_ref(arglist.v.list[2].v.list[matches[i]]));
     }
     free_var(keys);
     free_var(arglist);
