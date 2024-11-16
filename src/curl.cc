16433e8c63a96b4d25f3883600109cb89cc2b47c
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-11-16 13:07:12 -0800

    Enhanced return value properties

--- src/curl.cc
+++ src/curl.cc
@@ -10,6 +10,7 @@
 #include "log.h"
 #include "background.h"
 #include "map.h"
+#include "list.h"
 
 static CURL *curl_handle = nullptr;
 
@@ -17,6 +18,11 @@ typedef struct CurlMemoryStruct {
     char *result;
     size_t size;
 } CurlMemoryStruct;
+
+typedef struct {
+    char **headers;
+    size_t num_headers;
+} CurlHeaderList;
 
 /**
  * Set or overwrite a header in the curl_slist.
@@ -75,13 +81,45 @@ CurlWriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
     return realsize;
 }
 
+static size_t CurlHeaderCallback(char *buffer, size_t size, size_t nitems, void *userdata) {
+    size_t realsize = size * nitems;
+    CurlHeaderList *header_list = (CurlHeaderList *)userdata;
+
+    char *header_line = (char *)malloc(realsize + 1);
+    if(header_line == NULL) {
+        errlog("Not enough memory for header line\n");
+        return 0;
+    }
+    memcpy(header_line, buffer, realsize);
+    header_line[realsize] = '\0';
+
+    char **new_headers = (char **)realloc(header_list->headers, (header_list->num_headers + 1) * sizeof(char *));
+    if(new_headers == NULL) {
+        free(header_line);
+        errlog("Not enough memory for header list\n");
+        return 0;
+    }
+
+    header_list->headers = new_headers;
+    header_list->headers[header_list->num_headers] = header_line;
+    header_list->num_headers++;
+
+    return realsize;
+}
+
 static void curl_thread_callback(Var arglist, Var *ret, void *extra_data)
 {
     CURL *curl_handle;
     CURLcode res;
     CurlMemoryStruct chunk;
+    CurlHeaderList header_list;
     struct curl_slist *headers = NULL;
+    struct curl_slist *cookies;
+    struct curl_slist *nc;
     long timeout = CURL_TIMEOUT;
+
+    header_list.headers = NULL;
+    header_list.num_headers = 0;
 
     // Get out total list of arguments
     int nargs = arglist.v.list[0].v.num;
@@ -96,6 +134,8 @@ static void curl_thread_callback(Var arglist, Var *ret, void *extra_data)
     curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, CurlWriteMemoryCallback);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, CurlHeaderCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&header_list);
 
     // This block of code reads our headers argument and writes those into the request here.
     headers = curl_slist_append(headers, "Content-Type: application/json");
@@ -138,6 +178,11 @@ static void curl_thread_callback(Var arglist, Var *ret, void *extra_data)
       make_error_map(E_INVARG, "Invalid HTTP Method Provided", ret);
       curl_easy_cleanup(curl_handle);
       curl_slist_free_all(headers);
+      curl_slist_free_all(cookies);
+      for (size_t i = 0; i < header_list.num_headers; i++) {
+          free(header_list.headers[i]);
+      }
+      free(header_list.headers);
       // Return control early, so the call doesn't actually fall-thru and call the curl handle.
       return;
     }
@@ -149,11 +194,90 @@ static void curl_thread_callback(Var arglist, Var *ret, void *extra_data)
     if (res != CURLE_OK) {
       make_error_map(E_INVARG, curl_easy_strerror(res), ret);
     } else {
-      *ret = str_dup_to_var(raw_bytes_to_binary(chunk.result, chunk.size));
-      oklog("CURL [%s]: %lu bytes retrieved from: %s\n", method, (unsigned long)chunk.size, arglist.v.list[1].v.str);
+        // Response keys
+        static const Var status_key = str_dup_to_var("status");
+        static const Var body_key = str_dup_to_var("body");
+        static const Var headers_key = str_dup_to_var("headers");
+        static const Var cookies_key = str_dup_to_var("cookies");
+
+        // Response values
+        Var statusCode;
+        statusCode.type = TYPE_INT;
+
+        // Assigning values from response
+        curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &statusCode.v.num);
+
+        // Process headers
+        Var headers_var = new_map();
+        for (size_t i = 0; i < header_list.num_headers; i++) {
+            char *header_line = header_list.headers[i];
+            // Skip leading whitespace
+            while (isspace((unsigned char)*header_line)) header_line++;
+
+            if (strlen(header_line) == 0) {
+                continue;
+            }
+
+            char *colon = strchr(header_line, ':');
+            if (colon) {
+                *colon = '\0';
+                char *key = header_line;
+                char *value = colon + 1;
+                // Skip leading whitespace in value
+                while (isspace((unsigned char)*value)) value++;
+                // Remove trailing whitespace from key
+                char *key_end = colon - 1;
+                while (key_end > key && isspace((unsigned char)*key_end)) {
+                    *key_end = '\0';
+                    key_end--;
+                }
+                // Remove trailing whitespace from value
+                char *value_end = value + strlen(value) - 1;
+                while (value_end > value && isspace((unsigned char)*value_end)) {
+                    *value_end = '\0';
+                    value_end--;
+                }
+                Var key_var = str_dup_to_var(key);
+                Var value_var = str_dup_to_var(value);
+                headers_var = mapinsert(headers_var, key_var, value_var);
+            } else {
+                // Remove trailing whitespace from status line
+                char *line_end = header_line + strlen(header_line) - 1;
+                while (line_end > header_line && isspace((unsigned char)*line_end)) {
+                    *line_end = '\0';
+                    line_end--;
+                }
+                Var key_var = str_dup_to_var(":status-line");
+                Var value_var = str_dup_to_var(header_line);
+                headers_var = mapinsert(headers_var, key_var, value_var);
+            }
+        }
+
+
+        // Process cookies
+        res = curl_easy_getinfo(curl_handle, CURLINFO_COOKIELIST, &cookies);
+        Var cookie_list_var = new_list(0);
+        nc = cookies;
+        while(nc) {
+            cookie_list_var = listappend(cookie_list_var, str_dup_to_var(nc->data));
+            nc = nc->next;
+        }
+
+        *ret = new_map();
+        *ret = mapinsert(*ret, var_ref(status_key), statusCode);
+        *ret = mapinsert(*ret, var_ref(body_key), str_dup_to_var(raw_bytes_to_binary(chunk.result, chunk.size)));
+        *ret = mapinsert(*ret, var_ref(headers_key), headers_var);
+        *ret = mapinsert(*ret, var_ref(cookies_key), cookie_list_var);
+
+        oklog("CURL [%s]: %lu bytes retrieved from: %s\n", method, (unsigned long)chunk.size, arglist.v.list[1].v.str);
     }
 
     curl_slist_free_all(headers);
+    for (size_t i = 0; i < header_list.num_headers; i++) {
+        free(header_list.headers[i]);
+    }
+    free(header_list.headers);
+    curl_slist_free_all(cookies);
     curl_easy_cleanup(curl_handle);
     free(chunk.result);
 }
